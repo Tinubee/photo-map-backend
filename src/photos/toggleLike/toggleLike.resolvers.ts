d85430888e9b8a925a6b8b5c8a0860e2abca0cf7
KIMHYOUNGMIN <rlagud2005@naver.com>
2023-02-14 12:45:49 +0900

    ✒️ Feat : Add photo like & unlike

---
+++ src/photos/toggleLike/toggleLike.resolvers.ts
@@ -0,0 +1,57 @@
+import { protextedResolvers } from "../../user/users.utils";
+
+export default {
+  Mutation: {
+    toggleLike: protextedResolvers(
+      async (_, { id }, { loggedInUser, client }) => {
+        const photo = await client.photo.findUnique({
+          where: {
+            id,
+          },
+        });
+        if (!photo) {
+          return {
+            ok: false,
+            error: "Photo not found",
+          };
+        }
+
+        if (!loggedInUser) return;
+
+        const likeWhere = {
+          photoId_userId: {
+            userId: loggedInUser.id,
+            photoId: id,
+          },
+        };
+
+        const like = await client.like.findUnique({
+          where: likeWhere,
+        });
+        if (like) {
+          await client.like.delete({
+            where: likeWhere,
+          });
+        } else {
+          await client.like.create({
+            data: {
+              user: {
+                connect: {
+                  id: loggedInUser.id,
+                },
+              },
+              photo: {
+                connect: {
+                  id: photo.id,
+                },
+              },
+            },
+          });
+        }
+        return {
+          ok: true,
+        };
+      }
+    ),
+  },
+};
